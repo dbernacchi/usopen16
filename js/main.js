4c71d32aa2b79dd77edf3cec47db600d62782a36
Daniel Lewis <actiondan@gmail.com>
2015-08-20 21:28:48 +0100

    Array.indexOf polyfill

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+// polyfill for Array.indexOf (unsupported by IE8)
+// https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
+if (!Array.prototype.indexOf) {
+  Array.prototype.indexOf = function(searchElement, fromIndex) {
+    if (this == null) throw new TypeError('"this" is null or not defined');
+    var O = Object(this);
+    var len = O.length >>> 0;
+    if (len === 0) return -1;
+    var n = +fromIndex || 0;
+    if (Math.abs(n) === Infinity) n = 0;
+    if (n >= len) return -1;
+    var k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+    while (k < len) {
+      if (k in O && O[k] === searchElement) return k;
+      k++;
+    }
+    return -1;
+  };
+}
+
+
+
 $(function() {
 
   FastClick.attach(document.body);
