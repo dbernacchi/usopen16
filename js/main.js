3351d93639e5aa847ddb07880bcbeedf6c732944
Todd Lekan <todd.lekan@gmail.com>
2015-08-23 19:30:27 -0500

    main.js heavily updated

--- js/main.js
+++ js/main.js
@@ -1,338 +1,363 @@
-var IE = (!! window.ActiveXObject && +(/msie\s(\d+)/i.exec(navigator.userAgent)[1])) || NaN;
+var IE = (!!window.ActiveXObject && +(/msie\s(\d+)/i.exec(navigator.userAgent)[1])) || NaN;
 if (IE < 9) {
-  document.documentElement.className += ' lt-ie9' + ' ie' + IE;
+	document.documentElement.className += ' lt-ie9' + ' ie' + IE;
 }
 if (IE < 8) {
-  alert('Your browser needs to be updated to view this website');
+	alert('Your browser needs to be updated to view this website');
 }
 
 // polyfill for Array.indexOf (unsupported by IE8)
 // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
 if (!Array.prototype.indexOf) {
-  Array.prototype.indexOf = function(searchElement, fromIndex) {
-    if (this == null) throw new TypeError('"this" is null or not defined');
-    var O = Object(this);
-    var len = O.length >>> 0;
-    if (len === 0) return -1;
-    var n = +fromIndex || 0;
-    if (Math.abs(n) === Infinity) n = 0;
-    if (n >= len) return -1;
-    var k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
-    while (k < len) {
-      if (k in O && O[k] === searchElement) return k;
-      k++;
-    }
-    return -1;
-  };
+	Array.prototype.indexOf = function(searchElement, fromIndex) {
+		if (this == null)
+			throw new TypeError('"this" is null or not defined');
+		var O = Object(this);
+		var len = O.length >>> 0;
+		if (len === 0)
+			return -1;
+		var n = +fromIndex || 0;
+		if (Math.abs(n) === Infinity)
+			n = 0;
+		if (n >= len)
+			return -1;
+		var k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+		while (k < len) {
+			if ( k in O && O[k] === searchElement)
+				return k;
+			k++;
+		}
+		return -1;
+	};
 }
-
 
 $(function() {
 
-  FastClick.attach(document.body);
+	FastClick.attach(document.body);
 
-  var $body = $('body'),
-      $grid = $('.grid'),
-      $header = $('.sub-header'),
-      $about = $('.about'),
-      $activeTile,
-      loading,
-      activeFilter = 0,
-      isMobile = isMobile();
+	var $body = $('body'), $grid = $('.grid'), $header = $('.sub-header'), $about = $('.about'), $activeTile, loading, activeFilter = 0, isMobile = isMobile();
 
-  $(document).ready(loadTiles);
-  $(window).scroll(infiniteScroll);
+	$(document).ready(loadTiles);
+	$(window).scroll(infiniteScroll);
 
-  var $fixedHeader = $header.clone(true),
-      isFixedHeaderActive = false;
+	var $fixedHeader = $header.clone(true), isFixedHeaderActive = false;
 
-  $fixedHeader.addClass('fixed');
-  $fixedHeader.find('.about').removeClass('extended');
-  $fixedHeader.insertAfter($header);
+	$fixedHeader.addClass('fixed');
+	$fixedHeader.find('.about').removeClass('extended');
+	$fixedHeader.insertAfter($header);
 
-  $(window).scroll(function() {
-    var scrollTop = $(window).scrollTop();
+	$(window).scroll(function() {
+		var scrollTop = $(window).scrollTop();
 
-    if (scrollTop > 320 && !isFixedHeaderActive) {
-      isFixedHeaderActive = true;
-      $fixedHeader.addClass('active');
-    }
+		if (scrollTop > 320 && !isFixedHeaderActive) {
+			isFixedHeaderActive = true;
+			$fixedHeader.addClass('active');
+		}
 
-    if (scrollTop <= 320 && isFixedHeaderActive) {
-      isFixedHeaderActive = false;
-      $fixedHeader.removeClass('active');
-    }
-  });
+		if (scrollTop <= 320 && isFixedHeaderActive) {
+			isFixedHeaderActive = false;
+			$fixedHeader.removeClass('active');
+		}
+	});
 
-  $('.about .title span').typed({
-    strings: [
-      'More excitement.',
-      'More stories.',
-      'More fun.',
-      'More than the score.'
-    ],
-    typeSpeed: 20,
-    startDelay: 200,
-    backDelay: 1000,
-    loop: true,
-    showCursor: false
-  });
+	$('.about .title span').typed({
+		strings : ['More excitement.', 'More stories.', 'More fun.', 'More than the score.'],
+		typeSpeed : 20,
+		startDelay : 200,
+		backDelay : 1000,
+		loop : true,
+		showCursor : false
+	});
 
-  $body.on('click', function(e) {
-    if (e.target === this && $activeTile) {
-      hideContent($activeTile);
-    }
-  });
+	$body.on('click', function(e) {
+		if (e.target === this && $activeTile) {
+			hideContent($activeTile);
+		}
+	});
 
-  // desktop filters
-  $('.filters li').on('click', function() {
-    activeFilter = parseInt($(this).data('tag'));
-    filterTiles(activeFilter);
-  });
+	// desktop filters
+	$('.filters li').on('click', function() {
+		activeFilter = parseInt($(this).data('tag'));
+		filterTiles(activeFilter);
+	});
 
-  // mobile filters
-  $('.filters-dropdown').on('change', function() {
-    activeFilter = parseInt($(this).val()) || 0;
-    filterTiles(activeFilter);
-  });
+	// mobile filters
+	$('.filters-dropdown').on('change', function() {
+		activeFilter = parseInt($(this).val()) || 0;
+		filterTiles(activeFilter);
+	});
 
-  function showContent($tile) {
-    var data = $tile.data('data');
+	function showContent($tile) {
+		var data = $tile.data('data');
 
-    $tile.addClass('active');
-    $activeTile = $tile;
+		$tile.addClass('active');
+		$activeTile = $tile;
 
-    createContent(data).appendTo($tile);
-    if (!isMobile) { scrollToContent(); }
-  }
+		createContent(data).appendTo($tile);
+		if (!isMobile) {
+			scrollToContent();
+		}
+	}
 
-  function hideContent($tile) {
-    $tile.removeClass('active');
-    $tile.find('.content').remove();
-    $activeTile = undefined;
-  }
+	function hideContent($tile) {
+		$tile.removeClass('active');
+		$tile.find('.content').remove();
+		$activeTile = undefined;
+	}
 
-  function scrollToContent() {
-    var $content = $activeTile.find('.content'),
-        windowHeight = $(window).height(),
-        headerHeight = 166;
-        contentHeight = $content.height(),
-        contentOffset = $content.offset().top,
-        scrollTop = contentOffset - (windowHeight - contentHeight + headerHeight) / 2;
+	function scrollToContent() {
+		var $content = $activeTile.find('.content'), windowHeight = $(window).height(), headerHeight = 166;
+		contentHeight = $content.height(), contentOffset = $content.offset().top, scrollTop = contentOffset - (windowHeight - contentHeight + headerHeight) / 2;
 
-    $('body,html').animate({ scrollTop : scrollTop }, 350);
-  }
+		$('body,html').animate({
+			scrollTop : scrollTop
+		}, 350);
+	}
 
-  function loadTiles() {
-    loading = true;
-    $.get('manifest.json', function(data) {
+	function loadTiles() {
+		loading = true;
+		
+		var d = new Date();
+		var t = d.getTime();
 
-      data.tiles = _.shuffle(data.tiles);
+		$.ajax("manifest.json?" + t).always(function(build) {
+			
+			getPage(build.responseText, 1);
+		});
+	}
 
-      $.each(data.tiles, function(i, tile) {
-        var $tile = createTile(tile);
-        if (tile.tags.indexOf(activeFilter) != -1) {
-          $tile.hide();
-        }
-        $tile.appendTo($grid);
-      });
-      loading = false;
-    });
-  }
+	function getPage(build, idx) {
 
-  function createTile(data) {
-    var $tile = $('<li>'),
-        $preview = $('<div class="preview">').appendTo($tile);
+		var url = 'assets/builds/' + build + '/page_' + idx + '.json';
 
-    if (data.type == 'image' || data.type == 'video') {
-      var $previewImage = $('<img>').attr('src', data.preview);
-      // preload image
-      $preview.hide();
-      $previewImage.on('load', function() {
-        $preview.show();
-      });
-      $previewImage.appendTo($preview);
-    }
+		$.ajax(url).always(function(data) {
 
-    if (data.type == 'insight') {
-      if (isMobile) {
-        var PREVIEW_SIZE = screen.width - 20;
-      } else {
-        var PREVIEW_SIZE = 250;
-      }
-      $(INSIGHTS.preview(data.content, PREVIEW_SIZE))
-        .appendTo($preview);
-    }
+			/*var data = "";
 
-    // play button
-    if (data.type == 'video' || data.type == 'insight') {
-      $('<div class="play">').appendTo($preview);
-    }
+			try {
 
-    // share button
-    $('<div class="share">').appendTo($preview);
+				data = JSON.parse(output);
 
-    // keep data attached for later use
-    $tile.data('data', data);
+			} catch(e) {
 
-    // bind events
-    $tile.on('click', '.preview', tileClicked);
+				console.log('Parse Error:');
+				console.log(url);
+				console.log(output);
+			}*/
 
-    return $tile;
-  }
+			//data.tiles = _.shuffle(data.tiles);
 
-  function tileClicked(event) {
-    var $this = $(this).parent(),
-        sameTile = $this.is($activeTile);
+			$.each(data.tiles, function(i, tile) {
+				var $tile = createTile(tile);
+				if (tile.tags.indexOf(activeFilter) != -1) {
+					$tile.hide();
+				}
+				$tile.appendTo($grid);
+			});
 
-    if (!isMobile) {
-      // hide current content
-      if ($activeTile) {
-        hideContent($activeTile);
-      }
-      // show content if not the same tile
-      if (!sameTile) {
-        showContent($this);
-      }
-    } else {
-      // on mobile only the share button opens the overlay
-      if ($(event.target).hasClass('share')) {
-        showContent($this);
-      } else {
-        var data = $this.data('data');
+			if (data.last_page) {
 
-        if (data.type == 'video') {
-          prefix = 'https://www.youtube.com/watch?v=';
-          window.open(prefix + data.content.id, '_blank');
-        }
+				loading = false;
+			} else {
 
-        if (data.type == 'insight') {
-          INSIGHTS.play(data.content, $this.find('canvas')[0]);
-        }
-      }
-    }
-  }
+				idx = idx + 1;
+				getPage(build, idx);
+			}
 
-  function createContent(data) {
-    var $content = $('<div class="content">'),
-        $close = $('<div class="close">').appendTo($content);
+		});
 
-    createShareButtons(data).appendTo($content);
-    createSponsorLogos().appendTo($content);
+	}
 
-    $close.on('click', function() {
-      hideContent($activeTile);
-    });
+	/*
+	 function loadTiles() {
+	 loading = true;
+	 $.get('manifest.json', function(data) {
 
-    if (!isMobile && data.type === 'image') {
-      $('<img>')
-        .attr('src', data.content.image)
-        .appendTo($content);
-    }
+	 data.tiles = _.shuffle(data.tiles);
 
-    if (!isMobile && data.type === 'video') {
-      $('<iframe width="380" height="380">')
-        .attr('src', 'https://www.youtube.com/embed/' + data.content.id + '?rel=0&controls=0&showinfo=0&autoplay=1')
-        .attr('frameborder', 0)
-        .attr('allowfullscreen', true)
-        .appendTo($content);
-    }
+	 $.each(data.tiles, function(i, tile) {
+	 var $tile = createTile(tile);
+	 if (tile.tags.indexOf(activeFilter) != -1) {
+	 $tile.hide();
+	 }
+	 $tile.appendTo($grid);
+	 });
+	 loading = false;
+	 });
+	 }
+	 */
 
-    if (!isMobile && data.type === 'insight') {
-      var canvas = INSIGHTS.play(data.content);
-      $(canvas).appendTo($content);
-    }
+	function createTile(data) {
+		var $tile = $('<li>'), $preview = $('<div class="preview">').appendTo($tile);
 
-    return $content;
-  }
+		if (data.type == 'image' || data.type == 'video' || IE == 8) {
+			var $previewImage = $('<img>').attr('src', 'assets/preview_image/' + data.preview_image);
+			// preload image
+			$preview.hide();
+			$previewImage.on('load', function() {
+				$preview.show();
+			});
+			$previewImage.appendTo($preview);
+		}
 
-  function createShareButtons(data) {
-    var $share = $('<div class="share-buttons"></div>'),
-        url = encodeURIComponent(data.url);
 
-    $('<a class="share-facebook" />')
-      .attr('href', 'https://www.facebook.com/sharer/sharer.php?u=' + url)
-      .on('click', popupwindow)
-      .appendTo($share);
+		if (data.type == 'insight' && IE != 8) {
+		
+			if (isMobile) {
+				var PREVIEW_SIZE = screen.width - 20;
+			} else {
+				var PREVIEW_SIZE = 250;
+			}
+			$(INSIGHTS.preview(data.content, PREVIEW_SIZE)).appendTo($preview);
+		}
 
-    $('<a class="share-twitter" />')
-      .attr('href', 'https://twitter.com/home?status=' + url)
-      .on('click', popupwindow)
-      .appendTo($share);
+		// play button
+		if (data.type == 'video' || data.type == 'insight') {
+			$('<div class="play">').appendTo($preview);
+		}
 
-    $('<a class="share-google" />')
-      .attr('href', 'https://plus.google.com/share?url=' + url)
-      .on('click', popupwindow)
-      .appendTo($share);
+		// share button
+		$('<div class="share">').appendTo($preview);
 
-    $('<a class="share-linkedin" />')
-      .attr('href', 'https://www.linkedin.com/shareArticle?mini=true&url=' + url)
-      .on('click', popupwindow)
-      .appendTo($share);
+		// keep data attached for later use
+		$tile.data('data', data);
 
-    return $share;
-  }
+		// bind events
+		$tile.on('click', '.preview', tileClicked);
 
-  function popupwindow(e) {
-    e.preventDefault();
-    console.log(e);
+		return $tile;
+	}
 
-    var url = e.target.href,
-        w = 500, h = 300,
-        left = screen.width / 2 - w / 2,
-        top = screen.height / 2 - h / 2;
+	function tileClicked(event) {
+		var $this = $(this).parent(), sameTile = $this.is($activeTile);
 
-    return window.open(url, null, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
-  }
+		if (!isMobile) {
+			// hide current content
+			if ($activeTile) {
+				hideContent($activeTile);
+			}
+			// show content if not the same tile
+			if (!sameTile) {
+				showContent($this);
+			}
+		} else {
+			// on mobile only the share button opens the overlay
+			if ($(event.target).hasClass('share')) {
+				showContent($this);
+			} else {
+				var data = $this.data('data');
 
-  function createSponsorLogos(data) {
-    var $logos = $('<div class="sponsor-logos"></div>');
+				if (data.type == 'video') {
+					prefix = 'https://www.youtube.com/watch?v=';
+					window.open(prefix + data.content.id, '_blank');
+				}
 
-    $('<a href="#" class="logo-us-open" />' ).appendTo($logos);
-    $('<a href="http://www.ibm.com/" target="_blank" class="logo-ibm" />' ).appendTo($logos);
+				if (data.type == 'insight') {
+					INSIGHTS.play(data.content, $this.find('canvas')[0]);
+				}
+			}
+		}
+	}
 
-    return $logos;
-  }
+	function createContent(data) {
+	
+		var $content = $('<div class="content">'), $close = $('<div class="close">').appendTo($content);
 
-  function infiniteScroll() {
-    var scrollTop = $(window).scrollTop(),
-        docHeight = $(document).height(),
-        winHeight = $(window).height(),
-        threshold = 50;
+		createShareButtons(data).appendTo($content);
+		createSponsorLogos().appendTo($content);
 
-    if (scrollTop >= (docHeight - winHeight - threshold)) {
-      // stop if another request is in progress
-      if (loading) return false;
-      // need to send last tile id in order to
-      // skip the tiles that are already loaded
-      //loadTiles();
-    }
-  }
+		$close.on('click', function() {
+			hideContent($activeTile);
+		});
 
-  function filterTiles(tag) {
-    // sync desktop and mobile filters
-    $('.filters li').siblings().removeClass('active');
-    $('.filters li[data-tag='+activeFilter+']').addClass('active');
-    $('.filters-dropdown').val(activeFilter);
-    // hide current content
-    if ($activeTile) {
-      hideContent($activeTile);
-    }
-    $grid.find('li').show();
-    if (tag != 0) {
-      $grid.find('li').each(function() {
-        var $this = $(this),
-            data = $this.data('data');
-        if (data && data.tags.indexOf(tag) === -1) {
-          $this.hide();
-        }
-      });
-    }
-    $body.animate({ scrollTop : 0 }, 350);
-  }
+		if (!isMobile && data.type === 'image') {
+			$('<img>').attr('src', 'assets/media/' + data.content.image).appendTo($content);
+		}
 
-  function isMobile() {
-    return screen.width < 700;
-  }
+		if (!isMobile && data.type === 'video') {
+			$('<iframe width="380" height="380">').attr('src', 'https://www.youtube.com/embed/' + data.content.id + '?rel=0&controls=0&showinfo=0&autoplay=1').attr('frameborder', 0).attr('allowfullscreen', true).appendTo($content);
+		}
+
+		if (!isMobile && data.type === 'insight') {
+			var canvas = INSIGHTS.play(data.content);
+			$(canvas).appendTo($content);
+		}
+
+		return $content;
+	}
+
+	function createShareButtons(data) {
+		
+		var $share = $('<div class="share-buttons"></div>'), url = encodeURIComponent(data.url);
+
+		$('<a class="share-facebook" />').attr('href', 'https://www.facebook.com/sharer/sharer.php?u=' + url).on('click', popupwindow).appendTo($share);
+
+		$('<a class="share-twitter" />').attr('href', 'https://twitter.com/home?status=' + url).on('click', popupwindow).appendTo($share);
+
+		$('<a class="share-google" />').attr('href', 'https://plus.google.com/share?url=' + url).on('click', popupwindow).appendTo($share);
+
+		$('<a class="share-linkedin" />').attr('href', 'https://www.linkedin.com/shareArticle?mini=true&url=' + url).on('click', popupwindow).appendTo($share);
+
+		return $share;
+	}
+
+	function popupwindow(e) {
+		e.preventDefault();
+		console.log(e);
+
+		var url = e.target.href, w = 500, h = 300, left = screen.width / 2 - w / 2, top = screen.height / 2 - h / 2;
+
+		return window.open(url, null, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+	}
+
+	function createSponsorLogos(data) {
+		var $logos = $('<div class="sponsor-logos"></div>');
+
+		$('<a href="#" class="logo-us-open" />').appendTo($logos);
+		$('<a href="http://www.ibm.com/" target="_blank" class="logo-ibm" />').appendTo($logos);
+
+		return $logos;
+	}
+
+	function infiniteScroll() {
+		var scrollTop = $(window).scrollTop(), docHeight = $(document).height(), winHeight = $(window).height(), threshold = 50;
+
+		if (scrollTop >= (docHeight - winHeight - threshold)) {
+			// stop if another request is in progress
+			if (loading)
+				return false;
+			// need to send last tile id in order to
+			// skip the tiles that are already loaded
+			//loadTiles();
+		}
+	}
+
+	function filterTiles(tag) {
+		// sync desktop and mobile filters
+		$('.filters li').siblings().removeClass('active');
+		$('.filters li[data-tag=' + activeFilter + ']').addClass('active');
+		$('.filters-dropdown').val(activeFilter);
+		// hide current content
+		if ($activeTile) {
+			hideContent($activeTile);
+		}
+		$grid.find('li').show();
+		if (tag != 0) {
+			$grid.find('li').each(function() {
+				var $this = $(this), data = $this.data('data');
+				if (data && data.tags.indexOf(tag) === -1) {
+					$this.hide();
+				}
+			});
+		}
+		$body.animate({
+			scrollTop : 0
+		}, 350);
+	}
+
+	function isMobile() {
+		return screen.width < 700;
+	}
 
 });
+
